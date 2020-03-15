1895ee99927ca950bf29954ca65905e987c92c75
Getulio Sánchez <valentin2507@gmail.com>
2020-03-15 15:39:00 -0400

    Issue #3110641 by neel24: Refactor base code

--- src/Controller/DiscordAuthController.php
+++ src/Controller/DiscordAuthController.php
@@ -2,87 +2,50 @@
 
 namespace Drupal\social_auth_discord\Controller;
 
-use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Messenger\MessengerInterface;
+use Drupal\Core\Render\RendererInterface;
 use Drupal\social_api\Plugin\NetworkManager;
+use Drupal\social_auth\Controller\OAuth2ControllerBase;
 use Drupal\social_auth\SocialAuthDataHandler;
-use Drupal\social_auth\SocialAuthUserManager;
+use Drupal\social_auth\User\UserAuthenticator;
 use Drupal\social_auth_discord\DiscordAuthManager;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Drupal\Core\Routing\TrustedRedirectResponse;
 use Symfony\Component\HttpFoundation\RequestStack;
-use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 
 /**
- * Returns responses for Social Auth Reddit module routes.
+ * Returns responses for Social Auth Discord routes.
  */
-class DiscordAuthController extends ControllerBase {
-
-  /**
-   * The network plugin manager.
-   *
-   * @var \Drupal\social_api\Plugin\NetworkManager
-   */
-  private $networkManager;
-
-  /**
-   * The user manager.
-   *
-   * @var \Drupal\social_auth\SocialAuthUserManager
-   */
-  private $userManager;
-
-  /**
-   * The discord authentication manager.
-   *
-   * @var \Drupal\social_auth_discord\DiscordAuthManager
-   */
-  private $discordManager;
-
-  /**
-   * Used to access GET parameters.
-   *
-   * @var \Symfony\Component\HttpFoundation\RequestStack
-   */
-  private $request;
-
-  /**
-   * The Social Auth Data Handler.
-   *
-   * @var \Drupal\social_auth\SocialAuthDataHandler
-   */
-  private $dataHandler;
+class DiscordAuthController extends OAuth2ControllerBase {
 
   /**
    * DiscordAuthController constructor.
    *
+   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
+   *   The messenger service.
    * @param \Drupal\social_api\Plugin\NetworkManager $network_manager
    *   Used to get an instance of social_auth_discord network plugin.
-   * @param \Drupal\social_auth\SocialAuthUserManager $user_manager
-   *   Manages user login/registration.
+   * @param \Drupal\social_auth\User\UserAuthenticator $user_authenticator
+   *   Used to manage user authentication/registration.
    * @param \Drupal\social_auth_discord\DiscordAuthManager $discord_manager
    *   Used to manage authentication methods.
    * @param \Symfony\Component\HttpFoundation\RequestStack $request
    *   Used to access GET parameters.
    * @param \Drupal\social_auth\SocialAuthDataHandler $data_handler
-   *   SocialAuthDataHandler object.
+   *   The Social Auth data handler.
+   * @param \Drupal\Core\Render\RendererInterface $renderer
+   *   Used to handle metadata for redirection to authentication URL.
    */
-  public function __construct(NetworkManager $network_manager,
-                              SocialAuthUserManager $user_manager,
+  public function __construct(MessengerInterface $messenger,
+                              NetworkManager $network_manager,
+                              UserAuthenticator $user_authenticator,
                               DiscordAuthManager $discord_manager,
                               RequestStack $request,
-                              SocialAuthDataHandler $data_handler) {
+                              SocialAuthDataHandler $data_handler,
+                              RendererInterface $renderer) {
 
-    $this->networkManager = $network_manager;
-    $this->userManager = $user_manager;
-    $this->discordManager = $discord_manager;
-    $this->request = $request;
-    $this->dataHandler = $data_handler;
-
-    // Sets the plugin id.
-    $this->userManager->setPluginId('social_auth_discord');
-
-    // Sets the session keys to nullify if user could not logged in.
-    $this->userManager->setSessionKeysToNullify(['access_token', 'oauth2state']);
+    parent::__construct('Social Auth Discord', 'social_auth_discord', $messenger,
+                        $network_manager, $user_authenticator, $discord_manager,
+                        $request, $data_handler, $renderer);
   }
 
   /**
@@ -90,110 +53,47 @@ class DiscordAuthController extends ControllerBase {
    */
   public static function create(ContainerInterface $container) {
     return new static(
+      $container->get('messenger'),
       $container->get('plugin.network.manager'),
-      $container->get('social_auth.user_manager'),
+      $container->get('social_auth.user_authenticator'),
       $container->get('social_auth_discord.manager'),
       $container->get('request_stack'),
-      $container->get('social_auth.data_handler')
+      $container->get('social_auth.data_handler'),
+      $container->get('renderer')
     );
-  }
-
-  /**
-   * Response for path 'user/login/discord'.
-   *
-   * Redirects the user to Discord for authentication.
-   */
-  public function redirectToDiscord() {
-    /* @var Wohali\OAuth2\Client\Provider\Discord false $discord */
-    $discord = $this->networkManager->createInstance('social_auth_discord')->getSdk();
-
-    // If discord client could not be obtained.
-    if (!$discord) {
-      drupal_set_message($this->t('Social Auth Discord not configured properly. Contact site administrator.'), 'error');
-      return $this->redirect('user.login');
-    }
-
-    // Destination parameter specified in url.
-    $destination = $this->request->getCurrentRequest()->get('destination');
-    // If destination parameter is set, save it.
-    if ($destination) {
-      $this->userManager->setDestination($destination);
-    }
-
-    // Discord service was returned, inject it to $discordManager.
-    $this->discordManager->setClient($discord);
-
-    // Generates the URL where the user will be redirected for Discord login.
-    // If the user did not have email permission granted on previous attempt,
-    // we use the re-request URL requesting only the email address.
-    $discord_login_url = $this->discordManager->getDiscordLoginUrl();
-
-    $state = $this->discordManager->getState();
-
-    $this->dataHandler->set('oauth2state', $state);
-
-    return new TrustedRedirectResponse($discord_login_url);
   }
 
   /**
    * Response for path 'user/login/discord/callback'.
    *
-   * Discord returns the user here after user has authenticated in Discord.
+   * Discord returns the user here after user has authenticated.
    */
   public function callback() {
-    // Checks if user cancel login via Discord.
-    $error = $this->request->getCurrentRequest()->get('error');
-    if ($error == 'access_denied') {
-      drupal_set_message($this->t('You could not be authenticated.'), 'error');
-      return $this->redirect('user.login');
+
+    // Checks if there was an authentication error.
+    $redirect = $this->checkAuthError();
+    if ($redirect) {
+      return $redirect;
     }
 
-    /* @var Wohali\OAuth2\Client\Provider\Discord|false $discord */
-    $discord = $this->networkManager->createInstance('social_auth_discord')->getSdk();
+    /** @var \Wohali\OAuth2\Client\Provider\DiscordResourceOwner|null $profile */
+    $profile = $this->processCallback();
 
-    // If Discord client could not be obtained.
-    if (!$discord) {
-      drupal_set_message($this->t('Social Auth Discord not configured properly. Contact site administrator.'), 'error');
-      return $this->redirect('user.login');
+    // If authentication was successful.
+    if ($profile !== NULL) {
+
+      // Gets (or not) extra initial data.
+      $data = $this->userAuthenticator->checkProviderIsAssociated($profile->getId()) ? NULL : $this->providerManager->getExtraDetails();
+
+      return $this->userAuthenticator->authenticateUser($profile->getUsername(),
+                                                        $profile->getEmail(),
+                                                        $profile->getId(),
+                                                        $this->providerManager->getAccessToken(),
+                                                        $profile->getAvatarHash(),
+                                                        $data);
     }
 
-    $state = $this->dataHandler->get('oauth2state');
-
-    // Retrieves $_GET['state'].
-    $retrievedState = $this->request->getCurrentRequest()->query->get('state');
-    if (empty($retrievedState) || ($retrievedState !== $state)) {
-      $this->userManager->nullifySessionKeys();
-      drupal_set_message($this->t('Discord login failed. Unvalid OAuth2 state.'), 'error');
-      return $this->redirect('user.login');
-    }
-
-    // Saves access token to session.
-    $this->dataHandler->set('access_token', $this->discordManager->getAccessToken());
-
-    $this->discordManager->setClient($discord)->authenticate();
-
-    // Gets user's info from Discord API.
-    if (!$discord_profile = $this->discordManager->getUserInfo()) {
-      drupal_set_message($this->t('Discord login failed, could not load Discord profile. Contact site administrator.'), 'error');
-      return $this->redirect('user.login');
-    }
-
-    // Store the data mapped with data points define is
-    // social_auth_discord settings.
-    $data = [];
-
-    if (!$this->userManager->checkIfUserExists($discord_profile->getId())) {
-      $api_calls = explode(PHP_EOL, $this->discordManager->getApiCalls());
-
-      // Iterate through api calls define in settings and try to retrieve them.
-      foreach ($api_calls as $api_call) {
-
-        $call = $this->discordManager->getExtraDetails($api_call);
-        array_push($data, $call);
-      }
-    }
-    // If user information could be retrieved.
-    return $this->userManager->authenticateUser($discord_profile->getUsername(), $discord_profile->getEmail(), $discord_profile->getId(), $this->discordManager->getAccessToken(), $discord_profile->getAvatarHash(), json_encode($data));
+    return $this->redirect('user.login');
   }
 
 }
--- src/Form/DiscordAuthSettingsForm.php
+++ src/Form/DiscordAuthSettingsForm.php
@@ -2,56 +2,14 @@
 
 namespace Drupal\social_auth_discord\Form;
 
-use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Path\PathValidatorInterface;
-use Drupal\Core\Routing\RequestContext;
-use Drupal\Core\Routing\RouteProviderInterface;
+use Drupal\Core\Url;
 use Drupal\social_auth\Form\SocialAuthSettingsForm;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Settings form for Social Auth Discord.
  */
 class DiscordAuthSettingsForm extends SocialAuthSettingsForm {
-
-  /**
-   * The request context.
-   *
-   * @var \Drupal\Core\Routing\RequestContext
-   */
-  protected $requestContext;
-
-  /**
-   * Constructor.
-   *
-   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
-   *   The configuration factory.
-   * @param \Drupal\Core\Routing\RouteProviderInterface $route_provider
-   *   Used to check if route exists.
-   * @param \Drupal\Core\Path\PathValidatorInterface $path_validator
-   *   Used to check if path is valid and exists.
-   * @param \Drupal\Core\Routing\RequestContext $request_context
-   *   Holds information about the current request.
-   */
-  public function __construct(ConfigFactoryInterface $config_factory, RouteProviderInterface $route_provider, PathValidatorInterface $path_validator, RequestContext $request_context) {
-    parent::__construct($config_factory, $route_provider, $path_validator);
-    $this->requestContext = $request_context;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container) {
-    // Instantiates this class.
-    return new static(
-    // Load the services required to construct this class.
-      $container->get('config.factory'),
-      $container->get('router.route_provider'),
-      $container->get('path.validator'),
-      $container->get('router.request_context')
-    );
-  }
 
   /**
    * {@inheritdoc}
@@ -80,7 +38,8 @@ class DiscordAuthSettingsForm extends SocialAuthSettingsForm {
       '#type' => 'details',
       '#title' => $this->t('Discord Client settings'),
       '#open' => TRUE,
-      '#description' => $this->t('You need to first create a Discord App at <a href="@discord-dev">@discord-dev</a>', ['@discord-dev' => 'https://discordapp.com/developers/applications/me/create']),
+      '#description' => $this->t('You need to first create a Discord App at <a href="@discord-dev">@discord-dev</a>',
+        ['@discord-dev' => 'https://discordapp.com/developers/applications/me/create']),
     ];
 
     $form['discord_settings']['client_id'] = [
@@ -104,46 +63,36 @@ class DiscordAuthSettingsForm extends SocialAuthSettingsForm {
       '#disabled' => TRUE,
       '#title' => $this->t('Authorized redirect URIs'),
       '#description' => $this->t('Copy this value to <em>Authorized redirect URIs</em> field of your Discord App settings.'),
-      '#default_value' => $GLOBALS['base_url'] . '/user/login/discord/callback',
+      '#default_value' => Url::fromRoute('social_auth_discord.callback')->setAbsolute()->toString(),
     ];
 
-    $form['discord_settings']['scopes'] = [
+    $form['discord_settings']['advanced'] = [
+      '#type' => 'details',
+      '#title' => $this->t('Advanced settings'),
+      '#open' => FALSE,
+    ];
+
+    $form['discord_settings']['advanced']['scopes'] = [
       '#type' => 'textarea',
       '#title' => $this->t('Scopes for API call'),
       '#default_value' => $config->get('scopes'),
-      '#description' => $this->t('Define additional requested scopes to make API calls. A full list of valid scopes and their description is available in the <a href="https://www.drupal.org/node/2936957">Social Auth Reddit guide</a>.'),
+      '#description' => $this->t('Define any additional scopes to be requested, separated by a comma (e.g.: guilds,connections).<br>
+                                  The scopes  \'identify\' and \'email\' are added by default and always requested.<br>
+                                  You can see the full list of valid scopes and their description <a href="@scopes">here</a>.',
+                                  ['@scopes' => 'https://discordapp.com/developers/docs/topics/oauth2#shared-resources-oauth2-scopes']),
     ];
 
-    $form['discord_settings']['api_calls'] = [
+    $form['discord_settings']['advanced']['endpoints'] = [
       '#type' => 'textarea',
       '#title' => $this->t('API calls to be made to collect data'),
-      '#default_value' => $config->get('api_calls'),
-      '#description' => $this->t('Define the API calls which will retrieve data from provider.'),
+      '#default_value' => $config->get('endpoints'),
+      '#description' => $this->t('Define the endpoints to be requested when user authenticates with Discord for the first time<br>
+                                  Enter each endpoint in different lines in the format <em>endpoint</em>|<em>name_of_endpoint</em>.<br>
+                                  <b>For instance:</b><br>
+                                  /users/@me/guilds|user_guilds<br>'),
     ];
 
     return parent::buildForm($form, $form_state);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function validateForm(array &$form, FormStateInterface $form_state) {
-
-    // Convert the string of space-separated scopes into an array.
-    $scopes = explode(" ", $form_state->getValue('scopes'));
-
-    // Define the list of valid scopes. This array does not contain the default scopes.
-    $valid_scopes = ['', 'bot', 'gdm.join', 'messages.read', 'rpc', 'rpc.api', 'rpc.notifications.read', 'webhook.incoming'];
-
-    // Check if input contains any invalid scopes.
-    for ($i = 0; $i < count($scopes); $i++) {
-      if (!in_array($scopes[$i], $valid_scopes, TRUE)) {
-        $contains_invalid_scope = TRUE;
-      }
-    }
-    if (isset($contains_invalid_scope)) {
-      $form_state->setErrorByName('scope', t('You have entered an invalid scope, or the scope entered may already have been set by default. Please check and try again, or read the Social Auth Discord docs.'));
-    }
   }
 
   /**
@@ -155,7 +104,7 @@ class DiscordAuthSettingsForm extends SocialAuthSettingsForm {
       ->set('client_id', trim($values['client_id']))
       ->set('client_secret', trim($values['client_secret']))
       ->set('scopes', $values['scopes'])
-      ->set('api_calls', $values['api_calls'])
+      ->set('endpoints', $values['endpoints'])
       ->save();
 
     parent::submitForm($form, $form_state);
